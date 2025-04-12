dc54437bdde8e7e7496808f19fd0a304fdf25d6e
PatricioRayan <rayanborussia@gmail.com>
2025-04-12 13:33:20 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    printf("Desafio de Cartas!\n");
-    printf("Novo commit\n");
+    printf("Estado Rio de Janeiro: \n");
+    scanf("%s", &R01);
+    Carta 1: 
+    Estado: R
+    Codigo da carta: R01
+    Nome da Cidade: Rio de Janeiro
+    População: 2.000.000
+    Área (em km²): 1200.25 km²
+    PIB: 300.50 bilhões de reais  
+    Número de Pontos Turísticos: 2
+
+    printf("Estado Minas Gerais: \n");
+    scanf("%s", &M02);
+    Carta 2:
+    Estado M
+    Codigo da carta: M02
+    Nome da Cidade: Belo Horizonte
+    População: 1.540.000
+    Área (em km²): 3500.56 km²
+    PIB: 200.31 bilhões de reais
+    Número de Pontos Turísticos: 5
+    
 }
